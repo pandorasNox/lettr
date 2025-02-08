e9f28c7bf07b43b83442644496245ef84b5e3ba6
pandorasnox <itspandorasnox@gmail.com>
2025-02-08 14:41:54 +0100

    feat: adapt tailwind config to use relative path to config file

--- web/app/tailwind.config.js
+++ web/app/tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
     // content: [],
     // content: ["/css/**/*.{html,js}"],
     // content: ["/templates/*"],
-    content: ["../pkg/router/routes/templates/**/*.{html,tmpl}"], //relative to where tailwind is executed
+
+    // * By default Tailwind resolves non-absolute content paths relative to the current working directory
+    //   ('relative' to where tailwind is executed), as of tailwind v3.4.3 (behaviour might change in next major according to tailwind docs)
+    //   see: https://tailwindcss.com/docs/content-configuration#using-relative-paths
+    // *  To resolve paths relative to the tailwind.config.js file, use the object notation
+    //    and for `content` configuration set the `relative` property to true (content: {relative: true})
+    content: {
+        relative: true, // indicates relative to 'tailwind.config.js' and not 'current working directory'
+        files: ["./../../pkg/router/routes/templates/**/*.{html,tmpl}"],
+    },
+    //
+    //
     safelist: [
         // 'bg-red-500',
         // 'text-3xl',
