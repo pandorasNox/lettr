987a2708fc122702f0fbd61e95f009ef7908e884
pandorasnox <itspandorasnox@gmail.com>
2025-04-30 10:13:42 +0200

    chore: add test for main run func

--- main_test.go
+++ main_test.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bytes"
 	iofs "io/fs"
+	"log"
+	"net/http"
+	"os"
 	"slices"
+	"strings"
+	"syscall"
 	"testing"
+	"time"
+
+	"github.com/pandorasNox/lettr/pkg/session"
 )
 
 // todo: test for ???:
@@ -43,4 +52,48 @@ func getAllFilenames(efs iofs.FS) (files []string, err error) {
 	}
 
 	return files, nil
+}
+
+func Test_run(t *testing.T) {
+	type args struct {
+		sigChan          chan os.Signal
+		shutdownDoneChan chan bool
+		server           *http.Server
+		sessions         session.Sessions
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantOutput string
+	}{
+		// test cases
+		{
+			args: args{
+				sigChan:          make(chan os.Signal, 1),
+				shutdownDoneChan: make(chan bool, 2),
+				server:           &http.Server{},
+				sessions:         session.NewSessions(),
+			},
+			wantOutput: "Graceful shutdown complete.",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var buf bytes.Buffer
+			log.SetOutput(&buf)
+			originalOutput := log.Writer()
+			defer func() { log.SetOutput(originalOutput) }()
+
+			tt.args.sigChan <- syscall.SIGTERM // keep in mind: this bypasses "signal.Notify" relay behaviour, on theory we can send anything here at this point
+			run(tt.args.sigChan, tt.args.shutdownDoneChan, tt.args.server, tt.args.sessions)
+
+			time.Sleep(1 * time.Second)
+
+			output := buf.String()
+			if !strings.Contains(output, tt.wantOutput) {
+				t.Errorf("shutdown not complete, got '%v', want '%v' values", output, tt.wantOutput)
+			}
+		})
+	}
 }
