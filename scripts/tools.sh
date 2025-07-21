db77df5b9836cc774288c0add4a62688d56f7f10
Robert Krüger <robert.krueger@turbinekreuzberg.com>
2025-07-21 08:04:19 +0000

    feat: add tool to run playwright with UI locally

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -295,6 +295,13 @@ func_renovate() {(
 
 )}
 
+func_playwright_ui() {(
+  docker compose \
+    --file tests/playwright/playwright.docker-compose.yml \
+    --file tests/playwright/playwright-ui-patch.docker-compose.yml \
+    up
+)}
+
 # -----------------------------------------------------------------------------
 
 #   up                ...
@@ -453,6 +460,12 @@ else
     if [ "$1" == "renovate" ]
     then
       func_renovate
+      exit 0;
+    fi
+
+    if [ "$1" == "playwright-ui" ]
+    then
+      func_playwright_ui
       exit 0;
     fi
 
