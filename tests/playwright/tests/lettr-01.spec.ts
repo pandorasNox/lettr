efda3f66cb1f5999ca645726fee165ee5ed28ef5
pandorasNox <itspandorasnox@gmail.com>
2025-07-23 09:22:33 +0200

    chore: format playwright test

--- tests/playwright/tests/lettr-01.spec.ts
+++ tests/playwright/tests/lettr-01.spec.ts
@@ -5,17 +5,17 @@ import { test, expect } from '@playwright/test';
 let jsErrors: { console: string[], page: string[]} = { console: [], page: []};
 
 test.beforeEach(async ({ page }) => {
-  jsErrors = { console: [], page: []};
+    jsErrors = { console: [], page: []};
 
-  page.on('console', msg => {
-    if (msg.type() === 'error') {
-      jsErrors.console.push(msg.text());
-    }
-  });
+    page.on('console', msg => {
+        if (msg.type() === 'error') {
+            jsErrors.console.push(msg.text());
+        }
+    });
 
-  page.on('pageerror', err => {
-    jsErrors.page.push(err.message);
-  });
+    page.on('pageerror', err => {
+        jsErrors.page.push(err.message);
+    });
 });
 
 test.afterEach(async () => {
@@ -24,23 +24,20 @@ test.afterEach(async () => {
 });
 
 test('has title', async ({ page }, testInfo) => {
-    console.log('Base URL:', testInfo.project.use?.baseURL);
-//   const appUrl = process.env.APP_URL
     // await page.goto('http://lettrapp.aliases.containernetwork/');
+    console.log('Base URL:', testInfo.project.use?.baseURL);
     await page.goto('/');
 
     await page.locator('#theme-toggle').click()
 
     await expect(page.locator('nav h1.pl-2')).toBeVisible();
-    await expect(page.locator('nav h1.pl-3')).toHaveCount(0);
-//   const navHeadlineText = await page.locator('nav h1.pl-3').textContent();
-  // Fill the input by targeting the label.
-//   await expect(page.locator('nav h1.pl-2')).toHaveText('lettr2');
+    await expect(page.locator('nav h1.pl-3')).toHaveCount(0); // non-existence check
+    // const navHeadlineText = await page.locator('nav h1.pl-3').textContent();
+    // Fill the input by targeting the label.
+    // await expect(page.locator('nav h1.pl-2')).toHaveText('lettr2');
     await expect(page.getByRole('heading', { name: 'lettr' })).toHaveText('lettr');
     await expect(page.getByRole('heading', { name: 'lettr' })).toBeVisible();
 
-  // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/lettr/);
+    // Expect a title "to contain" a substring.
+    // await expect(page).toHaveTitle(/lettr/);
 });
-
-// `nav h1.pl-2`
