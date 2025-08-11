f618a5564defdd11dec4e2e04ca4653638930d5c
Robert Krüger <robert.krueger@turbinekreuzberg.com>
2025-08-11 07:57:49 +0000

    feat: validate playwright tests using typescript tsc

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -20,13 +20,11 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html", { open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    strict: true,
-
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.LETTR_APP_BASE_URL || 'https://localhost:9026',
 
